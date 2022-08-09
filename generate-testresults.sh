7b6fdb505f66d185c1a75e84a3622c45f176c22f
Guido Demmenie <30650086+guidodemmenie@users.noreply.github.com>
2022-08-09 18:17:24 +0200

    removed config

--- generate-testresults.sh
+++ generate-testresults.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
-source config.sh
+
+host=172.17.0.3
+user=testers
+pw=
+database=test-archive
 
 resultdir=`pwd`"/results"
 
 buildnr=20280014
 timestep=0
 
-for i in {1..50}
+echo host: $host
+echo user: $user
+echo database: $database
+
+for i in {1..30}
 do
-    # if i = 35 then
-        # robot -d results -i Team1 ./tst
-    # else
-        robot -d results ./tst
-    # fi
+    if [ $i = 8 ]; then
+       robot -d results -i Team1 ./tst
+    else
+       robot -d results ./tst
+    fi
     testarchiver --time-adjust-secs $timestep  \
-                 --dbengine postgresql \
-                 --host $host \
-                 --port 5432 \
-                 --team AutRegTeam \
-                 --series Regressie#$buildnr.001 \
-                 --dont-require-ssl \
-                 --user $user \
-                 --pw $pw \
-                 --database $database \
-                 $resultdir/output.xml
+          --dbengine postgresql \
+          --host $host \
+          --port 5432 \
+          --team AutRegTeam \
+          --series Regressie#$buildnr.001 \
+          --dont-require-ssl \
+          --user $user \
+          --pw $pw \
+          --database $database \
+          $resultdir/output.xml
 
     buildnr=$((buildnr + 1)) 
     timestep=$((timestep - 86400))
